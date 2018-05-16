4341b265346eece23bf88c5124ca78305dcd32c3
Karol Kawski <karol.kawski@protonmail.com>
2018-05-17 00:42:37 +0200

    refactor vol2, delete warrings

--- create.php
+++ create.php
@@ -1,26 +1,34 @@
 <?php
+    $mydata = json_decode(file_get_contents("DATA.json"), true);
+
     echo "<h2>Create</h2>";
     echo "<p><a href='index.php'>Back</a></p>";
-    $mydata = json_decode(file_get_contents("DATA.json"), true);
-    echo "<form method='post'><label>First name<input type='text' name='first_name' value=''></label></br>";
-    echo "<label>Last name <input type='text' name='last_name' value=''></label></br>";
-    echo "<label>Email <input type='text' name='email' value=''></label></br>";
-    echo "<label>IP address <input type='text' name='ip_address' value=''></label></br>";
-    echo "<label>Gender </label></br><label><input type='checkbox' name='gender' value='Male'>Male</label><label><input type='checkbox' name='gender' value='Female'></label>Female</br>";
-    echo "<input type='submit' name='submit' value='Create'></form>";
+
+    echo "<form method='post'><label>First name<input type='text' name='first_name' value=''></label></br>".
+        "<label>Last name <input type='text' name='last_name' value=''></label></br>".
+        "<label>Email <input type='text' name='email' value=''></label></br>".
+        "<label>IP address <input type='text' name='ip_address' value=''></label></br>".
+        "<label>Gender </label></br><label><input type='checkbox' name='gender' value='Male'>Male</label><label><input type='checkbox' name='gender' value='Female'></label>Female</br>".
+        "<input type='submit' name='submit' value='Create'></form>";
     
-    $id = count($mydata)+1;
-    $firstname = $_POST['first_name'];
-    $lastname = $_POST['last_name'];
-    $email = $_POST['email'];
-    $gender = $_POST['gender'];
-    $ip_address = $_POST['first_name'];
-    echo "<h3>Created user:</h3></br>";
-    echo "<table><tr><th>id</th>";
-    echo "<th>first_name</th>";
-    echo "<th>last_name</th>";
-    echo "<th>email</th>";
-    echo "<th>gender</th>";
-    echo "<th>ip_address</th></tr>";
-    echo "<tr><td>".$id."</td><td>".$firstname."</td><td>".$lastname."</td><td>".$email."</td><td>".$gender."</td><td>".$ip_address."</td></tr></table>";
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (isset($_POST['submit'])) {
+            $id = count($mydata)+1;
+            $firstname = $_POST['first_name'];
+            $lastname = $_POST['last_name'];
+            $email = $_POST['email'];
+            $gender = $_POST['gender'];
+            $ip_address = $_POST['first_name'];
+            echo "<h3>Created user:</h3></br>".
+                "<table><tr><th>id</th>".
+                "<th>first_name</th>".
+                "<th>last_name</th>".
+                "<th>email</th>".
+                "<th>gender</th>".
+                "<th>ip_address</th></tr>".
+                "<tr><td>".$id."</td><td>".$firstname."</td><td>".$lastname."</td><td>".$email."</td><td>".$gender."</td><td>".$ip_address."</td></tr></table>";
+        }
+    }
+
 ?>
--- search.php
+++ search.php
@@ -1,27 +1,31 @@
 <?php
     $mydata = json_decode(file_get_contents("DATA.json"), true);
-    $tosearch = $_POST['search'];
+ 
 
     echo "<h2>Search</h2>";
     echo "<p><a href='index.php'>Back</a></p>";
     echo "<form method='post'><input type='text' name='search' value=''><input type='submit' name='submit' value='Search'></form>";
 
-    echo "<table><tr><th>id</th>".
-        "<th>first_name</th>".
-        "<th>last_name</th>".
-        "<th>email</th>".
-        "<th>gender</th>".
-        "<th>ip_address</th></tr>";
-
-    foreach ($mydata as $val) {
-        if ($val["id"] == $tosearch or $val["first_name"] == $tosearch or $val["last_name"] == $tosearch or $val["email"] == $tosearch or $val["gender"] == $tosearch or $val["ip_address"] == $tosearch){
-        print_r("<tr><td>" . $val["id"] . "</td>");
-        print_r("<td>" . $val["first_name"] . "</td>");
-        print_r("<td>" . $val["last_name"] . "</td>");
-        print_r("<td>" . $val["email"] . "</td>");
-        print_r("<td>" . $val["gender"] . "</td>");
-        print_r("<td>" . $val["ip_address"] . "</td>");
-        echo "<td><a href='edit.php'>Edit</a></td></tr>";
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (isset($_POST['submit'])) {
+               $tosearch = $_POST['search'];
+                echo "<table><tr><th>id</th>".
+                    "<th>first_name</th>".
+                    "<th>last_name</th>".
+                    "<th>email</th>".
+                    "<th>gender</th>".
+                    "<th>ip_address</th></tr>";
+                foreach ($mydata as $val) {
+                    if ($val["id"] == $tosearch or $val["first_name"] == $tosearch or $val["last_name"] == $tosearch or $val["email"] == $tosearch or $val["gender"] == $tosearch or $val["ip_address"] == $tosearch){
+                    print_r("<tr><td>" . $val["id"] . "</td>");
+                    print_r("<td>" . $val["first_name"] . "</td>");
+                    print_r("<td>" . $val["last_name"] . "</td>");
+                    print_r("<td>" . $val["email"] . "</td>");
+                    print_r("<td>" . $val["gender"] . "</td>");
+                    print_r("<td>" . $val["ip_address"] . "</td>");
+                    echo "<td><a href='edit.php'>Edit</a></td></tr>";
+                    }
+                }
         }
     }
         echo "</table>";
